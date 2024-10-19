8ca10a050cde039c551b708e2be2e8cbf181f0b0
PoqaTJ <Tbjohnson42@gmail.com>
2024-10-19 12:55:36 -0700

    Update profile_manager.gd

--- scripts/managers/profile_manager.gd
+++ scripts/managers/profile_manager.gd
@@ -3,16 +3,22 @@ extends Node
 var points:int = 0
 
 func _ready() -> void:
-	pass	
+	pass
 
 func get_points() -> int:
-	return 0
+	return points
 
 func add_points(value:int) -> void:
-	return
+	if value <= 0:
+		print("ERROR: profile_manager.add_points() with non-positive value " + str(value))
+	points += value
+	points = max(0, points)
 
 func has_points(value:int) -> bool:
-	return false
+	return points >= value
 
 func spend_points(value:int) -> bool:
-	return false
+	if !has_points(value):
+		return false
+	points -= value
+	return true
